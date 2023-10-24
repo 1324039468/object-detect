529d4a67f14abcc26faf3153fc4457dd2da42c41
feifeifei_ <49754101+1324039468@users.noreply.github.com>
2023-10-24 11:27:03 +0800

    Create CAM.py

---
+++ Attention Modules for improve YOLO/CAM.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+
+
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
+    
+class CAM(nn.Module):
+    def __init__(self, inc, fusion='weight'):
+        super().__init__()
+        
+        assert fusion in ['weight', 'adaptive', 'concat']
+        self.fusion = fusion
+        
+        self.conv1 = Conv(inc, inc, 3, 1, None, 1, 1)
+        self.conv2 = Conv(inc, inc, 3, 1, None, 1, 3)
+        self.conv3 = Conv(inc, inc, 3, 1, None, 1, 5)
+        
+        self.fusion_1 = Conv(inc, inc, 1)
+        self.fusion_2 = Conv(inc, inc, 1)
+        self.fusion_3 = Conv(inc, inc, 1)
+
+        if self.fusion == 'adaptive':
+            self.fusion_4 = Conv(inc * 3, 3, 1)
+    
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.conv2(x)
+        x3 = self.conv3(x)
+        
+        if self.fusion == 'weight':
+            return self.fusion_1(x1) + self.fusion_2(x2) + self.fusion_3(x3)
+        elif self.fusion == 'adaptive':
+            fusion = torch.softmax(self.fusion_4(torch.cat([self.fusion_1(x1), self.fusion_2(x2), self.fusion_3(x3)], dim=1)), dim=1)
+            x1_weight, x2_weight, x3_weight = torch.split(fusion, [1, 1, 1], dim=1)
+            return x1 * x1_weight + x2 * x2_weight + x3 * x3_weight
+        else:
+            return torch.cat([self.fusion_1(x1), self.fusion_2(x2), self.fusion_3(x3)], dim=1)
+
+
+if __name__ == '__main__':
+    input = torch.randn(1, 128, 64, 64)
+    cam = CAM(128)
+    output = cam(input)
+    print(output.shape)
