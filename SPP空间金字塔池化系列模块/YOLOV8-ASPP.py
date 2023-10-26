8f75c487410c41f7651305460d097fc3523f1b7e
feifeifei_ <49754101+1324039468@users.noreply.github.com>
2023-10-26 10:22:27 +0800

    Create YOLOV8-ASPP.py

---
+++ SPP空间金字塔池化系列模块/YOLOV8-ASPP.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+class ASPP(nn.Module):
+    def __init__(self, in_channel=512, out_channel=256):
+        super(ASPP, self).__init__()
+        self.mean = nn.AdaptiveAvgPool2d((1, 1))  # (1,1)means ouput_dim
+        self.conv = nn.Conv2d(in_channel, out_channel, 1, 1)
+        self.atrous_block1 = nn.Conv2d(in_channel, out_channel, 1, 1)
+        self.atrous_block6 = nn.Conv2d(in_channel, out_channel, 3, 1, padding=6, dilation=6)
+        self.atrous_block12 = nn.Conv2d(in_channel, out_channel, 3, 1, padding=12, dilation=12)
+        self.atrous_block18 = nn.Conv2d(in_channel, out_channel, 3, 1, padding=18, dilation=18)
+        self.conv_1x1_output = nn.Conv2d(out_channel * 5, out_channel, 1, 1)
+
+    def forward(self, x):
+        size = x.shape[2:]
+
+        image_features = self.mean(x)
+        image_features = self.conv(image_features)
+        image_features = F.upsample(image_features, size=size, mode='bilinear')
+
+        atrous_block1 = self.atrous_block1(x)
+        atrous_block6 = self.atrous_block6(x)
+        atrous_block12 = self.atrous_block12(x)
+        atrous_block18 = self.atrous_block18(x)
+
+        net = self.conv_1x1_output(torch.cat([image_features, atrous_block1, atrous_block6,
+                                              atrous_block12, atrous_block18], dim=1))
+        return net
+
+
+if __name__ == '__main__':
+    x = torch.randn(1, 256, 16, 16)
+    model = ASPP(256, 256)
+    print(model(x).shape)
