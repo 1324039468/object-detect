d05a238042e72db55d003f2eb4836fb0ddf26c7f
feifeifei_ <49754101+1324039468@users.noreply.github.com>
2023-11-10 10:14:16 +0800

    Create YOLOV8-AFPN.py

---
+++ Some modules for improve YOLO/YOLOV8-AFPN.py
@@ -0,0 +1,176 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd
+from ultralytics.nn.modules.block import C2f
+
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
+
+
+
+
+class Upsample(nn.Module):
+    def __init__(self, in_channels, out_channels, scale_factor=2):
+        super(Upsample, self).__init__()
+
+        self.upsample = nn.Sequential(
+            Conv(in_channels, out_channels, 1),
+            nn.Upsample(scale_factor=scale_factor, mode='bilinear')
+        )
+
+        # carafe
+        # from mmcv.ops import CARAFEPack
+        # self.upsample = nn.Sequential(
+        #     BasicConv(in_channels, out_channels, 1),
+        #     CARAFEPack(out_channels, scale_factor=scale_factor)
+        # )
+
+    def forward(self, x):
+        x = self.upsample(x)
+
+        return x
+
+
+class Downsample(nn.Module):
+    def __init__(self, in_channels, out_channels, scale_factor=2):
+        super(Downsample, self).__init__()
+
+        self.downsample = nn.Sequential(
+            Conv(in_channels, out_channels, scale_factor, scale_factor, 0)
+        )
+
+    def forward(self, x):
+        x = self.downsample(x)
+
+        return x
+
+
+class ASFF_2(nn.Module):
+    def __init__(self, inter_dim=512, level=0, channel=[256, 512]):
+        super(ASFF_2, self).__init__()
+
+        self.inter_dim = inter_dim
+        compress_c = 8
+
+        self.weight_level_1 = Conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_2 = Conv(self.inter_dim, compress_c, 1, 1)
+
+        self.weight_levels = nn.Conv2d(compress_c * 2, 2, kernel_size=1, stride=1, padding=0)
+
+        self.conv = Conv(self.inter_dim, self.inter_dim, 3, 1)
+        self.upsample = Upsample(channel[1], channel[0])
+        self.downsample = Downsample(channel[0], channel[1])
+        self.level = level
+        self.c2f = C2f(inter_dim, inter_dim)
+    def forward(self, x):
+        input1, input2 = x
+        if self.level == 0:
+            input2 = self.upsample(input2)
+        elif self.level == 1:
+            input1 = self.downsample(input1)
+
+        level_1_weight_v = self.weight_level_1(input1)
+        level_2_weight_v = self.weight_level_2(input2)
+
+        levels_weight_v = torch.cat((level_1_weight_v, level_2_weight_v), 1)
+        levels_weight = self.weight_levels(levels_weight_v)
+        levels_weight = F.softmax(levels_weight, dim=1)
+
+        fused_out_reduced = input1 * levels_weight[:, 0:1, :, :] + \
+                            input2 * levels_weight[:, 1:2, :, :]
+
+        out = self.conv(fused_out_reduced)
+
+        return self.c2f(out)
+
+
+class ASFF_3(nn.Module):
+    def __init__(self, inter_dim=512, level=0, channel=[64, 128, 256]):
+        super(ASFF_3, self).__init__()
+
+        self.inter_dim = inter_dim
+        compress_c = 8
+
+        self.weight_level_1 = Conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_2 = Conv(self.inter_dim, compress_c, 1, 1)
+        self.weight_level_3 = Conv(self.inter_dim, compress_c, 1, 1)
+
+        self.weight_levels = nn.Conv2d(compress_c * 3, 3, kernel_size=1, stride=1, padding=0)
+
+        self.conv = Conv(self.inter_dim, self.inter_dim, 3, 1)
+
+        self.level = level
+        if self.level == 0:
+            self.upsample4x = Upsample(channel[2], channel[0], scale_factor=4)
+            self.upsample2x = Upsample(channel[1], channel[0], scale_factor=2)
+        elif self.level == 1:
+            self.upsample2x1 = Upsample(channel[2], channel[1], scale_factor=2)
+            self.downsample2x1 = Downsample(channel[0], channel[1], scale_factor=2)
+        elif self.level == 2:
+            self.downsample2x = Downsample(channel[1], channel[2], scale_factor=2)
+            self.downsample4x = Downsample(channel[0], channel[2], scale_factor=4)
+        self.c2f = C2f(inter_dim, inter_dim)
+
+    def forward(self, x):
+        input1, input2, input3 = x
+        if self.level == 0:
+            input2 = self.upsample2x(input2)
+            input3 = self.upsample4x(input3)
+        elif self.level == 1:
+            input3 = self.upsample2x1(input3)
+            input1 = self.downsample2x1(input1)
+        elif self.level == 2:
+            input1 = self.downsample4x(input1)
+            input2 = self.downsample2x(input2)
+        level_1_weight_v = self.weight_level_1(input1)
+        level_2_weight_v = self.weight_level_2(input2)
+        level_3_weight_v = self.weight_level_3(input3)
+
+        levels_weight_v = torch.cat((level_1_weight_v, level_2_weight_v, level_3_weight_v), 1)
+        levels_weight = self.weight_levels(levels_weight_v)
+        levels_weight = F.softmax(levels_weight, dim=1)
+
+        fused_out_reduced = input1 * levels_weight[:, 0:1, :, :] + \
+                            input2 * levels_weight[:, 1:2, :, :] + \
+                            input3 * levels_weight[:, 2:, :, :]
+
+        out = self.conv(fused_out_reduced)
+
+        return self.c2f(out)
+
+
+if __name__ == '__main__':
+
+    input_0 = torch.randn(1, 256, 40, 40)
+    input_1 = torch.randn(1, 512, 20, 20)
+    input = [input_0, input_1]
+    model1 = ASFF_2(256, level=0, channel=[256, 512])
+    output = model1(input)
+    print(output.shape)
+
+    input_0 = torch.randn(1, 256, 80, 80)
+    input_1 = torch.randn(1, 512, 40, 40)
+    input_2 = torch.randn(1, 1024, 20, 20)
+    input = [input_0, input_1, input_2]
+    model2 = ASFF_3(1024, level=2, channel=[256, 512, 1024])
+    output = model2(input)
+    print(output.shape)
